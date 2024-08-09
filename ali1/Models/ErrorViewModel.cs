a0417792e04d56abb1a1e89f38013a4c02b6259f
angel <angel@ANGELIS-LAPTOP>
2024-08-09 17:08:40 -0400

    Add project files.

---
+++ ali1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ali1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
